f5328ae529eb0b1c17ade1fccc60ea8756e5e403
Igor <77195831+igordavalos@users.noreply.github.com>
2021-02-28 14:13:18 +0100

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -35,7 +35,7 @@ resource "azurerm_linux_virtual_machine" "idavalosVM" {
     }
 
     boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+        storage_account_uri = azurerm_storage_account.idavalosstAccount.primary_blob_endpoint
     }
 
     tags = {
